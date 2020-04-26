b22ce07791d986ae1ccef6e2631dd90e88ca94ef
Anna <134panka@gmail.com>
2020-04-26 15:40:54 +0200

    topological sort

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from topological_sort import *
 import numpy as np
 from mst import *
 
-def main():
+def main_mst():
     filenames = ["data/mst_list70.txt","data/mst_matrix70.txt"]
     saturation = 60
     probes = 3
@@ -20,5 +20,20 @@ def main():
         print(n)
     return 0
 
+def main_ts():
+    filenames = ["data/ts_matrix.txt", "data/ts_list.txt"]
+    saturation = 60
+    probes = 3
+    for n in range(100, 1000, 20):
+        timel = 0
+        timem = 0
+        for a in range(probes):
+            matrix, list = generateDAG(n, saturation)
+            timem += measure_time(DFSmatrix, matrix, 0)
+            timel += measure_time(DFSlist, list, 0)
+        export_data_to_file(filenames[0], timem/probes)
+        export_data_to_file(filenames[1], timel/probes)
+    return 0
+
 if __name__ == '__main__':
-    main()
+    main_ts()
